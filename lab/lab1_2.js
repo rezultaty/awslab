6fd86770acae8788bdac52bddde27bd2a6ba1631
patryk_rutkowski <FryndzeL120#@!>
2017-04-05 01:05:49 +0200

    Próba pobrania informacji na temat instancji

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -3,8 +3,31 @@ var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
 
+
 var task =  function(request, callback){
-	
+    var ec2 = new AWS.EC2();
+
+    var params = {
+  DryRun: true,
+  Filters: [
+    {
+      Name: 'patrykrutkowski',
+      Values: [
+      ]
+    },
+    /* more items */
+  ],
+  InstanceIds: [
+    'i-092aa9e9b4a7959c3',
+    /* more items */
+  ],
+  MaxResults: 1,
+  NextToken: 'STRING_VALUE'
+};
+ec2.describeInstances(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 	
 }
 
