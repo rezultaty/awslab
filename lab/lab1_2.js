08ea12c088a24cd91058963adfe275678ea8eab7
patryk_rutkowski <FryndzeL120#@!>
2017-04-10 00:12:21 +0200

    Zrobione zadanie 2

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -2,33 +2,19 @@
 var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
+var ec2 = new AWS.EC2();
 
-
-var task =  function(request, callback){
-    var ec2 = new AWS.EC2();
-
-    var params = {
-  DryRun: true,
-  Filters: [
-    {
-      Name: 'patrykrutkowski',
-      Values: [
-      ]
-    },
-    /* more items */
-  ],
-  InstanceIds: [
-    'i-092aa9e9b4a7959c3',
-    /* more items */
-  ],
-  MaxResults: 1,
-  NextToken: 'STRING_VALUE'
+var params = {
+    DryRun: false,
+    InstanceIds: [
+        'i-092aa9e9b4a7959c3'
+    ]
 };
-ec2.describeInstances(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-	
+var task = function (request, callback) {
+    ec2.describeInstances(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else callback(null, data);           // successful response
+    });
 }
 
-exports.lab = task
+    exports.lab = task;
