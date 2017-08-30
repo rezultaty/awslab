92599b2108f14d526efaf4d2401499d109b72b21
patryk_rutkowski <FryndzeL120#@!>
2017-08-30 22:06:28 +0200

    Dodanie statycznych plików

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var example_1 = require("./example_1").lab;
 var PORT = 8080;
 
 var urlMap = [
-	{path: "/", action:__dirname + "/static/index.html"},	 
+	{path: "/", action:__dirname + "/static/index.html"},	
+	{path: "/assets", action:__dirname + "/static/assets"},
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1},
 	{path: "/awsinfo", action: lab1_2}
